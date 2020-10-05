b0be0c94570e85c08c7dfdaf048fecfac3c7875a
gingu-code <48529532+gingu-code@users.noreply.github.com>
2020-10-05 11:32:53 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// Create directory called fs-try
+
+// - Using fs module:
+// - create a directory called content using fs method and it should log 'content folder created'
+// - create a file using fs method called randomText.txt that lives outside the content directory
+// - randomText.txt should be created using fs method and given just a short string of any data you want to put in it, using fs method
+// - when you create the randomText.txt file you should also log 'randomtext.txt created' in the terminal.
+// - write the randomText.txt data to a new file called verbage.txt inside the content folder and log 'verbage.txt created'
+// - Now create a separate setTimeout function that after 7 seconds deletes the content directory
